873cb3418af4dc5cb9e85b9a57bfa0bd1d14f482
Zac Edens <edens.zac@gmail.com>
2021-12-14 14:52:15 -0800

    nextjs rehydrating now works properly! yay

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,56 @@
-import Document, { Html, Head, NextScript, Main } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+// import Document, { Html, Head, NextScript, Main } from "next/document";
+// import { ServerStyleSheet } from "styled-components";
+
+// export default class MyDocument extends Document {
+//   static getInitialProps({ renderPage }) {
+//     const sheet = new ServerStyleSheet();
+//     const page = renderPage(
+//       (App) => (props) => sheet.collectStyles(<App {...props} />)
+//     );
+//     const styleTags = sheet.getStyleElement();
+//     return { ...page, styleTags };
+//   }
+
+//   render() {
+//     return (
+//       <Html lang='en-US'>
+//         <Head />
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(
-      (App) => (props) => sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
-  }
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
 
-  render() {
-    return (
-      <Html lang='en-US'>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
 }
